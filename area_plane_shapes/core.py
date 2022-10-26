a1beaf6aeee6d3b31ef8e183029ca02093a1b78e
ericsalesdeandrade <sdaeric19@gmail.com>
2022-10-26 07:34:59 -0300

    Code formatting

--- area_plane_shapes/core.py
+++ area_plane_shapes/core.py
@@ -7,7 +7,8 @@ logging.basicConfig(level=logging.INFO)
 
 class AreaPlaneShapes:
     def __init__(self, radius=None, base: int | float = None,
-                 height: int | float = None, side: int | float = None) -> None:
+                 height: int | float = None, side: int | float = None) \
+            -> None:
         """
         Function to initialise the Area of Plane Shapes Class
         https://www.mathsisfun.com/area.html
@@ -23,25 +24,30 @@ class AreaPlaneShapes:
         if isinstance(radius, int | float | None):
             self.radius = radius
         else:
-            raise ValueError("Input `radius` should be of Type - Int or Float")
+            raise ValueError("Input `radius` should be of "
+                             "Type - Int or Float")
 
         # Validate `base` and `height` of type int | float
-        if isinstance(base, int | float | None) and isinstance(height, int | float | None):
+        if isinstance(base, int | float | None) and \
+                isinstance(height, int | float | None):
             self.base = base
             self.height = height
         else:
-            raise ValueError("Inputs `base` and `height` should be of Type - Int or Float")
+            raise ValueError("Inputs `base` and `height` "
+                             "should be of Type - Int or Float")
 
             # Validate `side` of type int | float
         if isinstance(side, int | float | None):
             self.side = side
         else:
-            raise ValueError("Input `side` should be of Type - Int or Float")
+            raise ValueError("Input `side` should be "
+                             "of Type - Int or Float")
 
     def area_of_triangle(self) -> str:
         """
         Function to calculate Area of Triangle
-        Required Parameters - Base and Height, Type - int | float
+        Required Parameters - Base and Height,
+            Type - int | float
        :return: Area, Type - str
         """
         try:
@@ -49,7 +55,8 @@ class AreaPlaneShapes:
                 area = round(0.5 * self.base * self.height, 2)
                 return f"The Area of the Triangle is `{area}` units"
             else:
-                return "Area Unavailable. Please initialise Class with `Base` and `Height`"
+                return "Area Unavailable. " \
+                       "Please initialise Class with `Base` and `Height`"
         except Exception as err:
             logging.error(err)
 
@@ -64,7 +71,8 @@ class AreaPlaneShapes:
                 area = round(math.pi * pow(self.radius, 2), 2)
                 return f"The Area of the Circle is `{area}` units"
             else:
-                return "Area Unavailable. Please initialise Class with `Radius`"
+                return "Area Unavailable. " \
+                       "Please initialise Class with `Radius`"
         except Exception as err:
             logging.error(err)
 
@@ -79,7 +87,8 @@ class AreaPlaneShapes:
                 area = round(pow(self.side, 2), 2)
                 return f"The Area of the Square is `{area}` units"
             else:
-                return "Area Unavailable. Please initialise Class with `Side`"
+                return "Area Unavailable. " \
+                       "Please initialise Class with `Side`"
         except Exception as err:
             logging.error(err)
 
@@ -94,7 +103,7 @@ class AreaPlaneShapes:
                 area = round(self.side * self.height, 2)
                 return f"The Area of the Rectangle is `{area}` units"
             else:
-                return "Area Unavailable. Please initialise Class with `Side` and `Height`"
+                return "Area Unavailable. " \
+                       "Please initialise Class with `Side` and `Height`"
         except Exception as err:
             logging.error(err)
-
